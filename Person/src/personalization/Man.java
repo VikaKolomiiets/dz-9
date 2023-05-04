4bc715d6ecaf1d55907c5f77bdba7d250de46260
Vika Kolomiets <vikakolomiets@gmail.com>
2023-05-04 17:31:23 +0300

    chore: added getFullInformation()

--- Person/src/personalization/Man.java
+++ Person/src/personalization/Man.java
@@ -1,12 +1,13 @@
 package personalization;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Man extends Person{
 
     private static final int RETIRED_AGE = 65;
-    private List<Person> partners;
+
 
     public Man(String firstName, String lastName, LocalDate dateOfBirth) throws Exception {
         super(firstName, lastName, dateOfBirth);
@@ -20,7 +21,27 @@ public class Man extends Person{
         return false;
     }
 
+    @Override
+    public String getFullInformation() {
+        StringBuilder stringBuilder = new StringBuilder(this.getFirstName() + " " + this.getLastName() + " is " + this.getFullAge() + " years old.");
+        switch ( this.getStatus() ){
+            case IS_MARRIED -> stringBuilder.append(" He is married with " + this.getPartner().getFirstName() + " " + this.getPartner().getLastName());
+            case SINGLE -> stringBuilder.append(" He is single.");
+            case IS_DIVORCED -> stringBuilder.append(" He is divorced.");
+            case WIDOWED -> stringBuilder.append(" He is widowed.");
+        }
+        if(this.getChildren().size() > 0){
+            stringBuilder.append(" And he has " + this.getChildren().size() + ".");
+        }
+        if (isRetired()){
+            stringBuilder.append(" he has been retired for " + (this.getFullAge() - RETIRED_AGE) + ".");
+        }
+        return stringBuilder.toString();
+    }
+
     public void createFamily(Woman newWife, boolean isChangeLastName, boolean isChangeLastNameNewWife) throws Exception {
         createFamilyInner(newWife, isChangeLastName, isChangeLastNameNewWife);
     }
+
+
 }
